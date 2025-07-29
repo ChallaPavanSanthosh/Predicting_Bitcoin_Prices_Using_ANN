047784a7e818ad17b6f29d464b29f975d5942141
ChallaPavanSanthosh <chvspsanthosh@gmail.com>
2025-07-29 23:49:36 +0530

    data_ingestion updated

--- src/ANNClassifier/components/data_ingestion.py
+++ src/ANNClassifier/components/data_ingestion.py
@@ -10,30 +10,6 @@ class DataIngestion:
     def __init__(self, config: DataIngestionConfig):
         self.config = config
 
-
-    # def download_file(self) -> str:
-    #     '''
-    #     Fetch data from the url
-    #     '''
-
-    #     try:
-    #         dataset_url = self.config.source_URL
-    #         download_path = self.config.local_data_file
-    #         os.makedirs("artifacts/data_ingestion", exist_ok=True)
-    #         logger.info(f"Downloading data from {dataset_url} into {download_path}")
-
-            
-    #         gdown.download(dataset_url, str(download_path), quiet=False, fuzzy=True)
-
-    #         file_id = dataset_url.split("/")[-2]
-    #         prefix = 'https://drive.google.com/uc?export=download&id='
-    #         gdown.download(prefix+file_id)
-
-    #         logger.info(f"Downloaded data from {dataset_url} into file {download_path}")
-
-    #     except Exception as e:
-    #         raise e
-
     def download_file(self) -> str:
         """
         Fetch data from the URL and save to local_data_file path.
